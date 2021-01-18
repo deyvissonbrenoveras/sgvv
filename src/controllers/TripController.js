d04008ccddc5619710ea2525d6628be7c7c49a16
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2021-01-18 14:39:20 -0300

    Senha do motorista e outras alterações

--- src/controllers/TripController.js
+++ src/controllers/TripController.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
 import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import bcrypt from 'bcryptjs';
 import Trip from '../models/Trip';
+import TripFilterEnum from '../util/TripFilterEnum';
 
 class TripController {
   async index(req, res) {
@@ -29,26 +31,50 @@ class TripController {
   }
 
   async show(req, res) {
-    const startTime = startOfDay(parseISO(req.query.startTime));
-    const endTime = endOfDay(parseISO(req.query.endTime));
-
-    const trips = await Trip.find({
-      startTime: {
-        $gte: startTime,
-      },
-      $or: [
-        {
-          endTime: {
-            $lt: endTime,
-          },
-        },
-        {
-          endTime: {
-            $exists: false,
-          },
-        },
-      ],
-    })
+    const status = Number(req.query.status);
+    const schema = Yup.object().shape({
+      // startTime: Yup.string()
+      //   .test('startTime', '', function checkStartTime(startTime) {
+      //     return !!Date.parse(startTime);
+      //   })
+      //   .notRequired()
+      //   .nullable(),
+      // endTime: Yup.string()
+      //   .nullable()
+      //   .test('endTime', '', function checkEndTime(endTime) {
+      //     const timeStamp = !!Date.parse(endTime);
+      //     return (timeStamp && timeStamp > 0) || undefined;
+      //   }),
+      // status: Yup.number().min(0).max(2).nullable().notRequired(),
+    });
+    if (!(await schema.isValid({ ...req.query, status }))) {
+      return res
+        .status(400)
+        .json({ error: 'Parâmetros de consulta inválidos' });
+    }
+    const query = {};
+    if (req.query.startTime) {
+      query.startTime = {
+        $gt: startOfDay(parseISO(req.query.startTime)),
+      };
+    }
+    if (req.query.endTime) {
+      query.endTime = {
+        $lt: endOfDay(parseISO(req.query.endTime)),
+      };
+    }
+    switch (status) {
+      case TripFilterEnum.IN_PROGRESS:
+        query.finished = false;
+        break;
+      case TripFilterEnum.FINISHED:
+        query.finished = true;
+        break;
+      default:
+        break;
+    }
+    console.log(query);
+    const trips = await Trip.find(query)
       .populate({
         path: 'driver',
         select: { name: 1 },
@@ -117,11 +143,11 @@ class TripController {
 
   async update(req, res) {
     const { _id } = req.params;
-    const { amountSpent } = req.body;
+    const { amountSpent, password } = req.body;
     const trip = await Trip.findById(_id)
       .populate({
         path: 'driver',
-        select: { name: 1 },
+        select: { name: 1, passwordHash: 1 },
         populate: {
           path: 'avatar',
           model: 'File',
@@ -140,12 +166,17 @@ class TripController {
     if (trip.finished) {
       return res.status(400).json({ error: 'Essa viagem já foi encerrada' });
     }
+    if (!bcrypt.compareSync(password || '', trip.driver.passwordHash)) {
+      return res.status(401).json({ error: 'Senha do motorista incorreta!' });
+    }
     trip.endTime = new Date(Date.now());
     trip.finished = true;
 
     trip.amountSpent = amountSpent;
     await trip.save();
-    return res.json(trip);
+    const resultTrip = trip.toObject();
+    delete resultTrip.driver.passwordHash;
+    return res.json(resultTrip);
   }
 }
 
